9c49e51434b0dd39635f293a7df878ef2acf4522
hshalash <hassan.shalash@arpuplus.com>
2023-04-20 18:01:24 +0200

    Gravity Flip(problem solving)

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int size =input.nextInt();
+        int arr [] = new int[size];
+         for (int i=0 ;i < size; i++){
+             arr[i]=input.nextInt();
+         }
+        Arrays.sort(arr);
+
+        for (int i=0 ;i < size; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
